cab98877d52fa30b97067e40fe6efb1797d27853
JhoneLee <JhoneLee@gmail.com>
2015-03-04 19:02:41 +0800

    add some code

---
+++ model/ECMAbase.js
@@ -0,0 +1,21 @@
+﻿ECMAbase = (functon(window){
+//Number extends
+
+//string extends
+//object extends
+//array extends
+
+/****随机排列数组****/
+/****万级数据操作耗时500-600ms左右***/
+    window.Array.prtotype.shuffle=function(){
+        var arr=[];
+        while(this.length>=0){
+            var rand=Math.floor(Math.random(0,this.length));
+            arr.push(this.splice(rand,1));
+        }
+        return arr;
+    };
+
+ 
+  return window;
+})(window);
